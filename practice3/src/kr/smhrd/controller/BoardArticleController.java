b7fc99a4c104d73c0b6ad8ed7139c79e0f19efa5
smhrd <smhrd@DESKTOP-DH1K0TN>
2021-11-04 12:49:36 +0900

    현준

--- practice3/src/kr/smhrd/controller/BoardArticleController.java
+++ practice3/src/kr/smhrd/controller/BoardArticleController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.smhrd.util.ArticleDAO;
-import kr.smhrd.util.CommentDAO;
 import kr.smhrd.util.SuperVO;
 
 public class BoardArticleController implements Controller{
@@ -16,16 +15,11 @@ public class BoardArticleController implements Controller{
 	@Override
 	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		int article_num = Integer.parseInt(request.getParameter("article_num"));
+		// TODO Auto-generated method stub
 		ArticleDAO dao = new ArticleDAO();
-		SuperVO vo = dao.article(article_num);
-		request.setAttribute("vo", vo);
-		
-		CommentDAO dao2 = new CommentDAO();
-		List<SuperVO> list = dao2.CommentList();
+		List<SuperVO> list = dao.Index();
 		request.setAttribute("list", list);
-		
+	
 		return "article";
 	}
 
