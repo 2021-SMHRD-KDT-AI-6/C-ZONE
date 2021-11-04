f8b1705b414665c4a4ab12081c5421a6645f9c65
smhrd.bsr@gmail.com <smhrd@220.80.65.223>
2021-11-04 10:54:09 +0900

    login 구현함 ㅎ

--- practice3/src/kr/smhrd/util/MbVO.java
+++ practice3/src/kr/smhrd/util/MbVO.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 public class MbVO {
     private String mb_id;
     private String mb_pwd;
-    private String mb_name;
     private Date mb_joindate;
     private String admin_yn;
     private String mb_profile_pic;
